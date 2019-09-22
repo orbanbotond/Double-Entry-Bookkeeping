4f774a8a93aad6a20c40e8e3976fa9577f52834f
Orban Botond <orbanbotond@gmail.com>
2019-09-22 23:25:52 +0300

    Added: engine to the routes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Borutus::Asset.create(:name => "Accounts Receivable")
+Borutus::Revenue.create(:name => "Sales Revenue")
+Borutus::Liability.create(:name => "Sales Tax Payable")
+entry = Borutus::Entry.build(
+          :description => "Sold some widgets",
+          :debits => [
+            {:account_name => "Accounts Receivable", :amount => 50}],
+          :credits => [
+            {:account_name => "Sales Revenue", :amount => 45},
+            {:account_name => "Sales Tax Payable", :amount => 5}])
+entry.save
+
