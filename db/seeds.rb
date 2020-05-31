9b90e83f7c78732e966d03c8d58d4d0ff3a2ef34
Orban Botond <orban@toptal.com>
2020-05-31 21:40:59 +0300

    Correct the seeds to Adhere to the Bookkeeping Equasion

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,27 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 Borutus::Asset.create(:name => "Real Estate")
 Borutus::Asset.create(:name => "Office")
-Borutus::Asset.create(:name => "Transport")
-Borutus::Equity.create(:name => "Equity Capital")
-Borutus::Equity.create(:name => "Working Capital")
-Borutus::Revenue.create(:name => "Investment")
-Borutus::Liability.create(:name => "Earned Premium")
+Borutus::Asset.create(:name => "Transport Vehicles")
+Borutus::Asset.create(:name => "Equity Capital")
+Borutus::Asset.create(:name => "Working Capital")
 Borutus::Asset.create(:name => "Accounts Receivable")
-Borutus::Revenue.create(:name => "Sales Revenue")
+
+Borutus::Liability.create(:name => "Investment")
 Borutus::Liability.create(:name => "Sales Tax Payable")
 
+Borutus::Revenue.create(:name => "Sales Revenue")
+
 entry = Borutus::Entry.new(
-    :description => "Bootstrap Injection",
-    :debits => [{:account_name => "Earned Premium", :amount => 10000.00}],
+    :description => "Bootstrap Capital Injection",
+    :debits => [{:account_name => "Equity Capital", :amount => 1000.00},
+                {:account_name => "Working Capital", :amount => 9000.00}],
     :credits => [{:account_name => "Investment", :amount => 10000.00}])
+entry.save
+
+entry = Borutus::Entry.new(
+    :description => "Transportation Vehicle Procurement",
+    :debits => [{:account_name => "Transport Vehicles", :amount => 3400.00}],
+    :credits => [{:account_name => "Working Capital", :amount => 3400.00}])
 entry.save
